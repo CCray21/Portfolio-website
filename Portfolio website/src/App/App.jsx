71f22832fbf1964d7834a2c10c39ee156f2f5ae8
CCray21 <charlie.bcray@gmail.com>
2025-07-09 00:00:13 +0100

    added section observer functionality

--- Portfolio website/src/App/App.jsx
+++ Portfolio website/src/App/App.jsx
@@ -1,10 +1,20 @@
+import {useEffect} from 'react';
 import Header from '../Header/Header.jsx'
 import Card from '../Profile card/Card.jsx';
 import styles from './App.module.css';
 import ProjectsSection from '../ProjectsSection/ProjectsSection.jsx';
 import ContactSection from '../ContactSection/ContactSection.jsx';
+import useSectionObserver from '../useSectionObserver.jsx';
 
 function App() {
+  const activeSection = useSectionObserver(['aboutMe','projects','contact']);
+  useEffect(() => {
+    let title = 'CCray ';
+    if (activeSection == 'aboutMe') title += '| About me';
+    if (activeSection == 'projects') title += '| Projects';
+    if (activeSection == 'contact') title += '| Contact';
+    document.title = title;
+  },[activeSection]);
   return(
     <>
     <section className={styles.header}>
---
+++ Portfolio website/src/useSectionObserver.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+export default function useSectionObserver(sectionIds) {
+  const [activeSection, setActiveSection] = useState('');
+
+  useEffect(() => {
+    const handleIntersect = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersect, {
+      threshold: 0.6,
+    });
+
+    sectionIds.forEach((id) => {
+      const section = document.getElementById(id);
+      if (section) observer.observe(section);
+    });
+
+    return () => {
+      sectionIds.forEach((id) => {
+        const section = document.getElementById(id);
+        if (section) observer.unobserve(section);
+      });
+    };
+  }, [sectionIds]);
+
+  return activeSection;
+}
